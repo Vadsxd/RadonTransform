b9a6d4b2a7fd7b50d8447aefe03fbae6f1a212bf
Vadsxd <vadim.sannikov.2018@mail.ru>
2022-05-12 23:37:06 +0300

    Изменил путь к папке для открытия файла

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PIL import Image
 from display_functions import *
 
 if __name__ == '__main__':
-    image = Image.open('/home/vadim/PycharmProjects/radiate/slphantom.png')
+    image = Image.open('/home/vadim/PycharmProjects/MathStaff/pictures/PictureExamp.png')
     animate_reconstruction(image)
     show_all_images(image)
 
