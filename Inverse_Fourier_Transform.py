9a55529d59a2839d574ffb1e1b7c1399af68caf3
Vadsxd <vadim.sannikov.2018@mail.ru>
2022-05-18 12:34:40 +0300

    Исправлена формула обратного Фурье

--- Inverse_Fourier_Transform.py
+++ Inverse_Fourier_Transform.py
@@ -5,5 +5,5 @@ def inverse_fourier_transform(x):
     N = x.shape[0]
     n = np.arange(N)
     k = n.reshape((N, 1))
-    M = np.exp(2j * np.pi * k * n / N)
+    M = np.exp(2j * np.pi * k * n / N) / N
     return np.dot(M, x)
